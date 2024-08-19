88434d9ffeb5f9842a89c875117fc4450e8724de
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-08-19 10:54:16 -0400

    Pass all tests

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,6 @@
 module ArtistsHelper
+  def display_artist(song)
+    return link_to "Add Artist", edit_song_path(song) if song.artist.nil?
+    link_to song.artist.name, artist_path(song.artist)
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,14 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    artist.name
   end
 
   def artist_name=(name)
+    if artist
+      artist.name = name
+    else 
+      build_artist(name: name)
+    end
   end
 end
